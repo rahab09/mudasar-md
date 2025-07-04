b13496fc18e58ed7a82c4bb834df81f2d1913230
SafuRaja7 <rajasaifi49@gmail.com>
2025-07-04 14:55:05 +0500

    minor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export function Header() {
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50);
+      setIsScrolled(window.scrollY > 40);
     };
 
     window.addEventListener('scroll', handleScroll);
